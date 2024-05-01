be4a0cf2be2cb5dc2778dad0ec103aaaa76d9875
ttttkx <144672418+ttttkx@users.noreply.github.com>
2024-05-01 11:49:43 +0800

    Update test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PATHONPATH=`pwd`
+# python -m pytest -s --ignore=fe/data
 coverage run --timid --branch --source fe,be --concurrency=thread -m pytest -v --ignore=fe/data
 coverage combine
 coverage report
