e6f427a2101d6a54f3f8707dad8cf5285bbc3627
ttttkx <144672418+ttttkx@users.noreply.github.com>
2024-05-01 11:29:05 +0800

    Update db_conn.py

--- be/model/db_conn.py
+++ be/model/db_conn.py
@@ -4,26 +4,29 @@ from be.model import store
 class DBConn:
     def __init__(self):
         self.client = self.conn = store.get_db_conn()
-
-        self.db = self.client["bookstore"]  # 切换到你的数据库
-        self.user_collection = self.db["user"]  # 获取 users 集合
-        self.store_collection = self.db["store"]  # 获取 store 集合
-        self.user_store_collection = self.db["user_store"]  # 获取 user_store 集合
+        
+        self.db = self.client["bookstore"] 
+        self.user_collection = self.db["user"]  
+        self.store_collection = self.db["store"]  
+        self.user_store_collection = self.db["user_store"] 
+        self.order_collection = self.db["new_order"]                    
 
     def user_id_exist(self, user_id):
-        # 查询用户是否存在
         query = {"user_id": user_id}
         user = self.user_collection.find_one(query)
         return user is not None
 
     def book_id_exist(self, store_id, book_id):
-        # 查询书籍是否存在于指定店铺
         query = {"store_id": store_id, "book_id": book_id}
-        book = self.conn['bookstore']['store'].find_one(query)
+        book = self.store_collection.find_one(query)
         return book is not None
 
     def store_id_exist(self, store_id):
-        # 查询店铺是否存在
         query = {"store_id": store_id}
         store = self.user_store_collection.find_one(query)
         return store is not None
+    
+    def order_id_exist(self, user_id, order_id):                    
+        query = {"user_id": user_id, "order_id": order_id}
+        order = self.order_collection.find_one(query)
+        return order is not None
